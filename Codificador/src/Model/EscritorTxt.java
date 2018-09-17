3f4821d8da4f58dccd0489e669e37cdde89a6b84
rees32 <reesedwin1194@gmail.com>
2018-09-16 19:13:32 -0600

    cambio direccion salida

--- Codificador/src/Model/EscritorTxt.java
+++ Codificador/src/Model/EscritorTxt.java
@@ -19,7 +19,7 @@ public class EscritorTxt implements IEscritor{
             }else{
                 modo = "Decodificacón";
             }
-        String ruta = "/Escritorio/"+modo+".txt";
+        String ruta = "ArchivosSalida/"+modo+".txt";
         try{
             File file = new File(ruta);
             if(!file.exists()){
