e6bba76774695ebd5435e0acb5c443f94d54ea1d
rees32 <reesedwin1194@gmail.com>
2018-09-29 21:45:17 -0600

    creando cascara del servidor

---
+++ Codificador/src/View/Servidor.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author reese
+ */
+public class Servidor {
+    
+    ServerSocket server;
+    Socket socket;
+    int puerto = 9090;
+    DataOutputStream salida;
+    BufferedReader entrada;
+    
+    public void iniciar(){
+        try {
+            server = new ServerSocket(puerto);
+            socket = new Socket();
+            socket = server.accept();
+            
+            entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String msj = entrada.readLine();
+            System.out.println(msj);
+            salida = new DataOutputStream(socket.getOutputStream());
+            salida.writeUTF("Recibido!");
+            socket.close();
+        } catch (Exception e) {
+        
+        }
+    }
+}
